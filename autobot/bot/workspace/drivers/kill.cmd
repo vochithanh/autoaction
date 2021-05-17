8d8d17f85613a3d6b396606fa412e1150b786abe
vochithanh <vochithanh.work@yahoo.com>
2021-05-17 11:55:21 +0700

    move out autoweb workspace

---
+++ autobot/bot/workspace/drivers/kill.cmd
@@ -0,0 +1,7 @@
+taskkill /F /IM geckodriver.exe
+taskkill /F /IM chromedriver.exe
+taskkill /F /IM microsoftwebdriver.exe
+taskkill /F /IM operadriver.exe
+#taskkill /F /IM MicrosoftEdge.exe
+#taskkill /F /IM chrome.exe
+#taskkill /F /IM firefox.exe
